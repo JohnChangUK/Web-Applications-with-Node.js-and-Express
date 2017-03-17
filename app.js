b691884e10ca54d15221ffa8923bc9ccb50c114d
John Chang <johnchang9094@gmail.com>
2017-03-17 17:01:03 +0000

    Routing finished, starting MongoDB for Database Usage

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
-
 var app = express();
 
+
 var port = process.env.PORT || 5000;
-var bookRouter = require('./src/routes/bookRoutes');
+
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+    }, {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -12,15 +21,17 @@ app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 
-app.get('/', function(req, res) {
-        res.render('index', {
-            title: "Hello from render", 
-            nav: [{
-                Link: '/Books', 
-                Text: 'Books'
-            }, {
-                Link: '/Authors', Text: 'Authors'}] 
-            });
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function(req, res) {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var bookRouter = express.Router();
 
+var router = function(nav) {
+
 var books = [
     {
         title: "Harry Potter",
@@ -25,22 +27,24 @@ var books = [
 
 bookRouter.route('/')
     .get(function(req, res) {
-    res.render('books', {
-            title: "Books", 
-            nav: [{
-                Link: '/Books', 
-                Text: 'Books'
-            }, {
-                Link: '/Authors', 
-                Text: 'Authors'
-            }],
+    res.render('bookListView', {
+            title: 'Books',
+            nav: nav,
             books: books
             });
-})
-
-bookRouter.route('/single')
-    .get(function(req, res) {
-    res.send('Hello Single Book');
 });
 
-module.exports = bookRouter;
+bookRouter.route('/:id')
+    .get(function (req, res) {
+        var id = req.params.id;
+        res.render('bookView', {
+            title: 'Books',
+            nav: nav,
+            book: books[id]
+        });
+    });
+    
+    return bookRouter;
+}
+
+module.exports = router;
