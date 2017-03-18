2591d1d1f7f5a0592bbd7477ee6b905769b23476
John Chang <johnchang9094@gmail.com>
2017-03-18 22:56:57 +0000

    Working API from goodreadI from GoodreadsService, HTTP and added xml2js

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,13 +1,34 @@
 var http = require('http');
 var xml2js = require('xml2js');
-
+var parser = xml2js.Parser({explicitArray: false});
 
 var goodreadsService = function() {
 
     var getBookById = function(id, cb) {
-      cb(null, {
-        description: "Our description"
-      });
+
+      var options = {
+        host: 'www.goodreads.com',
+        path: '/book/show/3.xml?key=xv6IbLoUZXFXCEsV2oTSQ'
+      };
+
+      var callback = function(response) {
+        var str = "";
+
+        response.on('data', function(chunk) {
+          str += chunk;
+        });
+        response.on('end', function() {
+          console.log(str);
+          parser.parseString(str, 
+            function(err, result) {
+              cb(null, 
+                result.GoodreadsResponse.book);
+
+          });
+        });
+      };
+    
+      http.request(options, callback).end();
     };
 
     return {
