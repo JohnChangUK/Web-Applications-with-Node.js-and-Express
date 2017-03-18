f4c42f7222e3e95d861cec7e4eb4d7be38875bf9
John Chang <johnchang9094@gmail.com>
2017-03-18 14:30:16 +0000

    Saving User Details

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,27 @@ var mongodb = require('mongodb').MongoClient;
       authRouter.route('/signUp')
         .post(function(req, res) {
           console.log(req.body);
+            var url = 
+            'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db) {
+              var collection = db.collection('users');
+              var user = {
+                username: req.body.userName,
+                password: req.body.password
+              };
+
+              collection.insert(user, 
+                function(err, results) {
+                  req.login(results.ops[0], function() {
+                    res.redirect('/auth/profile');
+                  });
+                });
+            });
+         
+        });
+        authRouter.route('/profile')
+        .get(function(req, res) {
+          res.json(req.user);
         });
       return authRouter;
   };
